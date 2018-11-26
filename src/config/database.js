7ee951e664ad797c829ae872ca21bac882ea9c80
root <root@m4n0l0>
2018-11-26 09:54:20 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'root',
+  database: 'gonode',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
